0e147c4f7b9606ffc4e2d15b9e39ac583e57fb2d
David Angel Alba Bonilla <78463209+DavidAlba2627@users.noreply.github.com>
2023-10-02 20:14:21 -0600

    Update visualize_results.py

--- Implementation/Code/visualize_results.py
+++ Implementation/Code/visualize_results.py
@@ -21,11 +21,11 @@ def plot_predictions(X, y_pred, title="Predictions"):
     plt.show()
 
 # Compare the predicted labels with the correct labels
-def compare_with_correct(X_test, y_test, y_pred):
+def compare_with_correct(X, y_true, y_pred):
     """
     Parameters:
-    - X_test: The test dataset.
-    - y_test: The correct labels.
+    - X: The dataset.
+    - y_true: The correct labels.
     - y_pred: The predicted labels.
     """
     
@@ -33,18 +33,18 @@ def compare_with_correct(X_test, y_test, y_pred):
     # Creating a figure and a set of subplots
     fig, ax = plt.subplots(1, 2, figsize=(14, 5))
     # Plotting the test set with the correct labels
-    scatter = ax[0].scatter(X_test[:, 0], X_test[:, 1], c=y_test, s=50, cmap=color_map, alpha=0.4)
+    scatter = ax[0].scatter(X[:, 0], X[:, 1], c=y_true, s=50, cmap=color_map, alpha=0.4)
     ax[0].legend(handles=scatter.legend_elements()[0], labels=['0', '1'], title='Classes')
     ax[0].set_title("Correct Classes", fontsize=18)
     # Creating a new color map for the predicted labels
     color_map2 = ListedColormap(['mediumseagreen', 'mediumblue', 'firebrick'])
     # Finding the indices of the points that were classified incorrectly
-    incorrect_indices = np.where(y_pred != y_test)[0]
+    incorrect_indices = np.where(y_pred != y_true)[0]
     # The labels of the points that were classified incorrectly are set to 2
     y_plot = y_pred.copy()
     y_plot[incorrect_indices] = 2
     # Plotting the test set with the predicted labels
-    scatter = ax[1].scatter(X_test[:, 0], X_test[:, 1], c=y_plot, s=50, cmap=color_map2, alpha=0.4)
+    scatter = ax[1].scatter(X[:, 0], X[:, 1], c=y_plot, s=50, cmap=color_map2, alpha=0.4)
     ax[1].legend(handles=scatter.legend_elements()[0], labels=['0', '1', 'Incorrect'], title='Classes')
     ax[1].set_title("Predicted Classes", fontsize=18)
     
